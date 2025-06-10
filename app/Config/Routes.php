0cff6768fdf862577625afbd1a87e09d43dd25ae
Aloysius Abian Dwi Hananta <aloysiusbian@gmail.com>
2025-06-10 22:56:12 +0700

    coba

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,15 +11,15 @@ $routes->get('/produk', 'ProdukController::index');
 $routes->get('/login', 'loginController::index');
 
 
-$routes->match(['get', 'post'], '/register', 'RegisterController::index');
+$routes->match(['get', 'post'], '/register', 'RegisterController::register');
+// $routes->match(['get', 'post'], 'register', 'RegisterController::index');
 
-$routes->match(['get', 'post'], 'register', 'RegisterController::index');
+// $routes->match(['get', 'post'], 'register', 'RegisterController::index');
 $routes->get('produk/detail/(:any)', 'ProdukController::detail/$1');
 
 
 
 
-$routes->match(['get', 'post'], '/register', 'RegisterController::register');
-// $routes->match(['get', 'post'], 'register', 'RegisterController::index');
+// $routes->match(['get', 'post'], '/register', 'RegisterController::register');
 $routes->get('produk/detail/(:any)', 'ProdukController::detail/$1');
 
