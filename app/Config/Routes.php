c1ea03331035d7e15aa38ebff0d9d7574ef5d4d0
Avarest1 <eza585824@gmail.com>
2025-06-12 00:04:21 +0700

    registrasi

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,12 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+$routes->match(['get', 'post'], '/register', 'UserController::register');
 $routes->match(['get', 'post'], '/', 'UserController::register');
 $routes->get('/beranda', 'BerandaController::index');
 
 $routes->get('/login', 'UserController::login');
 $routes->post('/login', 'UserController::auth'); // tetap butuh ini
 $routes->get('/logout', 'UserController::logout');
+$routes->get('/register', 'UserController::register');
+$routes->post('/register', 'UserController::register');
 
 
 $routes->get('/produk', 'ProdukController::index');
