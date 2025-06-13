5dae8091840f6abad4ce91b6d1ab409216d4608a
Avarest1 <eza585824@gmail.com>
2025-06-13 18:03:29 +0700

    fix

--- app/Controllers/RiwayatController.php
+++ app/Controllers/RiwayatController.php
@@ -9,13 +9,8 @@ class RiwayatController extends BaseController
     public function index(): string
     {
         $model = new PembayaranModel();
+        $riwayat = $model->orderBy('created_at', 'DESC')->findAll();
 
-<<<<<<< HEAD
-        $data['riwayat'] = $model->orderBy('created_at', 'DESC')->findAll();
-
-        return view('riwayat', $data);
-=======
         return view('riwayat', ['riwayat' => $riwayat]);
->>>>>>> 62dca86749ef226867ac89f1da8c5b7ee30ef6ea
     }
 }
